1cae0b31a740644fca6013be6ffdcc13dc958aa5
TengGit <codebackup6422@gmail.com>
2020-03-19 21:07:10 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const classNames = {
+  TODO_ITEM: 'todo-container',
+  TODO_CHECKBOX: 'todo-checkbox',
+  TODO_TEXT: 'todo-text',
+  TODO_DELETE: 'todo-delete',
+}
+
+const list = document.getElementById('todo-list')
+const itemCountSpan = document.getElementById('item-count')
+const uncheckedCountSpan = document.getElementById('unchecked-count')
+
+function newTodo() {
+  let list = []
+  let todoItem = prompt("Enter your todo item")
+  list.push(todoItem)
+  console.log(list)
+
+}
